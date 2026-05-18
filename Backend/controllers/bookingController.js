8eea556e97525dafd2eb192e7a4a2ba72b8b24e8
Atalwiar077 <Atalwiar145@gmail.com>
2026-05-19 01:19:17 +0430

    the otp route is created for booking

--- Backend/controllers/bookingController.js
+++ Backend/controllers/bookingController.js
@@ -1,7 +1,21 @@
 import Booking from "../model/Booking.js";
-import { protect, admin } from "../middleware/auth.js";
+import OTP from "../model/OTP.js";
+import User from "../model/User.js";
+import Event from "../model/Event.js";
+import { sendEmail, sendBookingEmail } from "../utils/email.js";
 
-const bookEvent = async (req, res) => {
-  const bookings = await Booking.find();
-  res.send(bookings);
-};
+const generateOTP = () => {
+  return Math.floor(100000 + Math.random() * 900000).toString();
+}
+
+export const sendBookingOTP = async (email, otp, type) => {
+  try {
+    const otp = generateOTP();
+    await OTP.findByIdAndDelete({email: req.user.email, action: "booking"});
+      await OTP.create({email: req.user.email, otp:otp, action: "booking"});
+      await sendEmail(req.user.email, otp, 'event booking');
+      res.json({message: "OTP sent successfully"});
+  } catch (error) {
+    console.log(error);
+  }
+}
